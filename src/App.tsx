630fc9ddea1b2e3853bb84b60ac478d97ac738c9
ImGabreuw <imgabreuw.dev@gmail.com>
2021-06-22 17:27:58 -0300

    Added authentication state recovery

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import {createContext, useState, useEffect} from "react";
 import {BrowserRouter, Route} from "react-router-dom";
 
 import {NewRoom} from "./pages/NewRoom";
@@ -20,6 +20,24 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
     const [user, setUser] = useState<User>();
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                const {displayName, photoURL, uid} = user;
+
+                if (!displayName || !photoURL) {
+                    throw new Error("Missing information from Google Account.")
+                }
+
+                setUser({
+                    id: uid,
+                    name: displayName,
+                    avatar: photoURL
+                })
+            }
+        })
+    }, [])
 
     async function signInWithGoogle() {
         const provider = new firebase.auth.GoogleAuthProvider();
