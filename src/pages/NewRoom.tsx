fcada16dbac9cbfa1e6fa1168f293f5b7b67e2b2
ImGabreuw <imgabreuw.dev@gmail.com>
2021-06-23 13:33:57 -0300

    Developed room creation

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,20 +1,38 @@
 import {Link} from "react-router-dom";
+import {FormEvent, useState} from "react"
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 
 import {Button} from "../components/Button";
-// import {useAuth} from "../hooks/useAuth";
+import {useAuth} from "../hooks/useAuth";
 
 import "../styles/auth.scss"
+import {database} from "../services/firebase";
 
 export function NewRoom() {
-    // const {user} = useAuth();
+    const {user} = useAuth();
+    const [newRoom, setNewRoom] = useState("");
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (newRoom.trim() === "") {
+            return
+        }
+
+        const roomRef = database.ref("rooms");
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        });
+    }
 
     return (
         <div id="page-auth">
             <aside>
-                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
@@ -22,8 +40,13 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask"/>
                     <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input type="text" placeholder="Nome da sala" />
+                    <form onSubmit={handleCreateRoom}>
+                        <input
+                            type="text"
+                            placeholder="Nome da sala"
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
+                        />
                         <Button type="submit">Criar sala</Button>
                     </form>
                     <p>
